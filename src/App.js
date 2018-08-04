f52edce806b0c2329ddf726b683b6e813afeedc1
Ricky Drohan <rdrohanirl@yahoo.ie>
2018-08-04 15:25:55 +0100

    Added Apollo code to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import 'grommet/grommet.min.css';
 
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <p>Application Here</p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="app">
+          <p>Application Here</p>
+        </div>
+      </ApolloProvider>
     );
   }
 }
