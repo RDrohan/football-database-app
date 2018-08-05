9acbe36371324ed56862db91e52485ca878fd166
Ricky Drohan <rdrohanirl@yahoo.ie>
2018-08-05 16:38:31 +0100

    Added React Router to application

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'grommet/grommet.min.css';
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './component/Home';
 
@@ -14,7 +15,11 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <Home />
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
